9f7df2d1af7b9eb8785ed22f587e1b0c1fc179e7
Colin <mcbolin@hotmail.com>
2024-06-21 21:10:19 +1000

    feat: types and names displayed correctly

--- src/pokemonApi.js
+++ src/pokemonApi.js
@@ -20,6 +20,20 @@ async function getPokemonData(){
 
 async function putDataOnPage(dataToDisplay){
     document.getElementsByClassName("pokemonName")[0].textContent = dataToDisplay.name;
+
+    let type1Display = document.getElementsByClassName("pokemonType1")[0];
+    let type2Display = document.getElementsByClassName("pokemonType2")[0];
+
+    type1Display.textContent = "Type 1: " + dataToDisplay.types[0].type.name;
+    // Could also be seen as = data.types[0]["type"]["name"]; - does the exact same thing
+
+    if (dataToDisplay.types[1]){
+        // if the data includes a 2nd type, set that as well
+        type2Display.textContent = "Type 2: " + dataToDisplay.types[1].type.name;
+    } else {
+        // if there is no 2nd type, clear the text content
+        type2Display.textContent = "";
+    }
 }
 
 // Button calls this
